65f61b2af88c439887bc017a48f9b96262e91a08
Jeffrey Schang <jschang@linkedin.com>
2012-06-06 08:13:55 -0700

    fix problem with native Java types

--- lib/jmx/mbean_proxy.rb
+++ lib/jmx/mbean_proxy.rb
@@ -129,6 +129,9 @@ module JMX
         required_type = JavaClass.for_name(type)
         
         java_arg = param.to_java(:object)
+        java_arg = param.to_java(Java::java.lang.Integer) if required_type.name == "int"
+        java_arg = param.to_java(Java::java.lang.Short) if required_type.name == "short"
+        java_arg = param.to_java(Java::java.lang.Float) if required_type.name == "float"
 
         if (param.kind_of? Array)
           java_arg = param.inject(ArrayList.new) {|l, element| l << element }
